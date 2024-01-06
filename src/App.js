f3ac1392b9e037b84630489b2e8ed857c289de60
Aman Raj <razzaman9834@gmail.com>
2024-01-06 18:59:10 +0530

    8plus8

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React {8+8}
         </a>
       </header>
     </div>
